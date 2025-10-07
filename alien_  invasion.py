c149e248eee4d9bb5474448a7fd2e3b111aab678
Manoj Reddy <manojreddypalla@gmail.com>
2025-10-07 20:37:02 +0530

    ff

--- alien_  invasion.py
+++ alien_  invasion.py
@@ -1,13 +1,16 @@
 import sys 
 import pygame 
-
+from settings import settings
 
 
 
 def run_game():
     #initilze the screen and create a screen object 
     pygame.init()
-    screen=pygame.display.set_mode((1200,800))
+    game_settings=settings()
+    screen=pygame.display.setmode(game_settings.screen_width,game_settings.screen_height)
+
+    
     pygame.display.set_caption("alian invation ")
     bg_color =(230,230,230)
     #set the background color 
@@ -27,7 +30,7 @@ def run_game():
 
 
 
-        screen.fill(bg_color)
+        screen.fill(game_settings.bg_color)
            # Make the most recently drawn screen visible.
         pygame.display.flip()        
 run_game()
