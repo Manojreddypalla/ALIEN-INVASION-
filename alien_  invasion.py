bd5bd8ab5fbb874b07e912e3c03a7986e317da10
Manoj Reddy <manojreddypalla@gmail.com>
2025-10-07 20:24:39 +0530

    initial

---
+++ alien_  invasion.py
@@ -0,0 +1,33 @@
+import sys 
+import pygame 
+
+
+
+
+def run_game():
+    #initilze the screen and create a screen object 
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("alian invation ")
+    bg_color =(230,230,230)
+    #set the background color 
+   
+
+
+
+
+#start the main loop buddy n
+#from here ok 
+
+    while True :
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+
+
+
+
+        screen.fill(bg_color)
+           # Make the most recently drawn screen visible.
+        pygame.display.flip()        
+run_game()
---
+++ setting.py
@@ -0,0 +1,9 @@
+class setting():
+    """a clsss to store all the settings for alien invation"""
+
+    def __inin__(self):
+        """initialize the game's static settings."""
+        #screen settings
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color(230,230,230)
