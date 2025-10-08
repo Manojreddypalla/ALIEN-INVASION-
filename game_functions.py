e3349d08ba89dd397a0932007d5b39034e1b9ac5
Manoj Reddy <manojreddypalla@gmail.com>
2025-10-08 23:49:34 +0530

    08

--- game_functions.py
+++ game_functions.py
@@ -5,14 +5,18 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type ==  pygame.QUIT:
             sys.exit()
+            # here we press the botton
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_RIGHT:
                 ship.moving_right=True
-
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left=True    
+             # here we realise the button 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right=False        
-
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 
